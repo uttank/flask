9e338645eb3230ffe0ec4f9476b1c369a8a96f0b
uttank <uttank@snu.ac.kr>
2022-08-23 12:14:14 +0900

    admin 에서 모델 추가 안되고 있음

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
+
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='CTP', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
+
+# Add administrative views here
 
 
 @app.route('/')
