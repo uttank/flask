e0685c69a532d0f05c5169ca2ae6c1f151e743ba
uttank <uttank@snu.ac.kr>
2022-10-25 19:21:43 +0900

    end of job

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_admin import Admin
 
 app = Flask(__name__)
+
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+# set flask_admin
+admin = Admin(app, name='CTP', template_mode='bootstrap3')
 
 
 @app.route('/')
@@ -11,3 +18,7 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+if __name__ == '__main__':
+    app.run()
